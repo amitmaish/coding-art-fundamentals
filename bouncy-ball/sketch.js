caf87cb533d0e49bb6331423539752426f38481e
amitmaish <83043520+AmitMaish@users.noreply.github.com>
2025-10-14 18:40:07 -0700

    added bouncy ball

---
+++ bouncy-ball/sketch.js
@@ -0,0 +1,72 @@
+const RADIUS = 10;
+const SPEED = RADIUS * 2;
+let x, y;
+let velocity;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  velocity = new Vec(random(-1, 1), random(-1, 1));
+  velocity.normalize();
+
+  x = width / 2;
+  y = height / 2;
+
+  colorMode(OKLCH);
+  background(30, 50, 4, 100);
+}
+
+function draw() {
+  background(30, 50, 4, 0.015);
+  moveCircle();
+
+  noStroke();
+  colorMode(OKLCH);
+  fill(random(60, 100), random(80, 100), random(0, 360));
+  circle(x, y, RADIUS * 2);
+
+  handleCollision();
+}
+
+function moveCircle() {
+  x += velocity.x * SPEED;
+  y += velocity.y * SPEED;
+}
+
+function handleCollision() {
+  if (RADIUS > x || x > windowWidth - RADIUS) {
+    velocity.flip_x();
+  }
+  if (RADIUS > y || y > windowHeight - RADIUS) {
+    velocity.flip_y();
+  }
+}
+
+class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  length2() {
+    return this.x ** 2 + this.y ** 2;
+  }
+
+  length() {
+    return sqrt(this.length2());
+  }
+
+  normalize() {
+    let length = this.length();
+    this.x = this.x / length;
+    this.y = this.y / length;
+  }
+
+  flip_x() {
+    this.x = -this.x;
+  }
+
+  flip_y() {
+    this.y = -this.y;
+  }
+}
